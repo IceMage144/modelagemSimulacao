a2585f53a71aa41fc989a8d803fd9b0f380719d0
robotenique <julianogarcia_1997@hotmail.com>
2017-04-08 13:16:23 -0300

    add annotations to MRU graph

--- EP1/EP1.py
+++ EP1/EP1.py
@@ -6,9 +6,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from pathlib import Path
 
-
-# Number of lines for each JSON entry
-ENTRY_LENGTH = 8
 class Walker:
     # Constant / static variable - MAY BE USEFUL..
     SPACE = 30
@@ -32,6 +29,7 @@ class Walker:
         return meanVel
 
     def __calculateVelocity(self, run):
+        # This should not be used for a MRUV run!
         msr = [float(m) for m in run['measures'].split("|") if m != '']
         # Alternate measure
         if(run['mType'] == 'A'):
@@ -44,31 +42,35 @@ class Walker:
             return sum((i/t for i, t in zip(range(10, 31, 10), msr)))/len(msr)
 
     def __spaceF(self, run):
-        simVel = 30/self.__finalTime(run)
+        # Return a closure in the form x(t)
+        simVel = Walker.SPACE/self.__finalTime(run)
         def f(t):
             return simVel*t
         return f
 
     def __timeList(self, run):
+        # Return the list of observed times. The length may vary
+        # if it's an Alternate or Normal read.
         msr = [float(m) for m in run['measures'].split("|") if m != '']
         if(run['mType'] == 'N'):
             msr = [(i+j)/2 for i, j in zip(msr[:len(msr)//2],msr[len(msr)//2:])]
         return msr
 
     def __spaceList(self, run):
-        t = "t"
+        # Return a list of spaces (e.g. [10, 20, 30]) and a list of
+        # space xticks (e.g. [t1, t2, t3]). The length changes if it's Alternate.
         if(run['mType'] == 'N'):
             spcList = list(range(10,35, 10))
-            xticks = [t+str(i) for i in range(1,4)]
+            xticks = ["t"+str(i) for i in range(1,4)]
         else:
             spcList = list(range(5, 35, 5))
-            xticks = [t+str(i) for i in range(1,7)]
-
+            xticks = ["t"+str(i) for i in range(1,7)]
 
         return [spcList, xticks]
 
 
     def __finalTime(self, run):
+        # Semantic way of getting the final time
         msr = self.__timeList(run)
         return msr[len(msr) - 1]
 
@@ -76,44 +78,73 @@ class Walker:
 
     def __plotCsv(self, run, axis):
         file = "./CSV/" + run['csv'] + ".csv"
-        labels = ['', 'Fx', 'Fy', 'Fz', 'Fr']
+        labels = ['Tempo', 'Fx', 'Fy', 'Fz', 'Fr']
         df = pd.read_csv(file, names=labels, sep=';', decimal=',')
-        df.plot(ax=axis)
+        df.plot(ax=axis, x = 'Tempo')
         ax = plt.gca()
-        ax.set_xticklabels([])
-        #TODO: change
-        axis.set_ylim(-0.5, 1.5)
-        axis.set_xlim(0,4332)
+        ax.set_yticklabels([])
+        ax.set_xlabel("")
+        plt.tick_params(axis='both', which='both', bottom='off',
+                        top='off', labelbottom='off', right='off',
+                        left='off', labelleft='off')
+        ax.axis('off')
+        plt.legend(loc='upper right')
+        axis.set_xlim(0, np.max(np.asarray(df['Tempo'])))
+
+    def __setAnnotations(self, xObs, yObs, labels, values, ax):
+        for x, y, text, val in zip(xObs, yObs, labels, values):
+            ax.annotate(text+f"{round(val, 2)}",
+            xy=(x, y + 1), xycoords='data',
+            xytext=(-15, 25), textcoords='offset points',
+            arrowprops=dict(facecolor='cyan', shrink=0.05),
+            horizontalalignment='right', verticalalignment='bottom')
+
+
+    def __calculateError(self, xObs, yObs, xt):
+        '''
+        Calculates the mean error of a list of points in the form (xObs, yObs) and
+        the points (xObs, xt(xObs)), where xt is a function provided by the
+        caller of the method.
+        '''
+        error = [abs(y - xt(x)) for x, y in  zip(xObs, yObs)]
+        return sum(error)/len(error)
+
 
 
     def plotGraph(self):
         for run in self.times:
             xt = self.__spaceF(run)
-            x = np.arange(0, self.__finalTime(run), 0.3)
+            x = np.asarray([0, self.__finalTime(run)])
             y = list(map(xt, x))
             realX = self.__timeList(run)
             realY = self.__spaceList(run)[0]
             timeNames = self.__spaceList(run)[1]
             labelDict = {tValue : tName for tValue, tName in zip(realX, timeNames)}
             xticks = [int(i) for i in np.arange(0, self.__finalTime(run), 5)]
-            xticks = sorted(xticks + list(labelDict.keys()))
+            #xticks = sorted(xticks + list(labelDict.keys()))
             labels = [str(i) for i in xticks]
-            labels = [labelDict.get(float(t), t) for t in labels]
+            #labels = [labelDict.get(float(t), t) for t in labels]
             f, axarr = plt.subplots(nrows=2, ncols=1, figsize=(15, 10))
-            axarr[0].plot(x, y)
+            axarr[0].plot(x, y, label = "simulado")
             for i, j in zip(list(labelDict.keys()), realY):
                 axarr[0].plot([i, i], [0, j], 'r--')
-            axarr[0].scatter(realX, realY,color='red', marker="+")
+            axarr[0].scatter(realX, realY,color='red', marker="+", label = "observado")
+            axarr[0].set_xlabel('tempo (s)')
+            axarr[0].set_ylabel('espaço (m)')
             axarr[0].set_title(run["csv"])
             axarr[0].set_xticklabels(labels)
             axarr[0].set_xticks(xticks)
+            axarr[0].spines['right'].set_visible(False)
+            axarr[0].spines['top'].set_visible(False)
             axarr[0].set_xlim(0, self.__finalTime(run) + 1)
             axarr[0].set_ylim(0, Walker.SPACE + 1)
-            self.__plotCsv(run, axarr[1])
-            removeP = lambda strg: strg.replace('(','').replace(')','')
-            plt.show()
+            axarr[0].legend(shadow=True)
+            self.__setAnnotations(realX, realY, list(labelDict.values()), list(labelDict.keys()), axarr[0])
 
-            #axarr[1].scatter(x, y)
+
+            self.__plotCsv(run, axarr[1])
+            plt.autoscale(False)
+            plt.show()
 
 
 
