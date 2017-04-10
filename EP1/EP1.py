290cf15a0bfc446a9bebc2b53de47dcb939a1300
robotenique <julianogarcia_1997@hotmail.com>
2017-04-10 16:50:52 -0300

    update the requirements and readability of the code

--- EP1/EP1.py
+++ EP1/EP1.py
@@ -1,9 +1,9 @@
+#python version = 3.6
 import sys
 import json
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
 def main():
     mVel = mAcc = cMRU = cMRUV = 0
     # Parse a json file passed as argument
@@ -19,6 +19,7 @@ def main():
     for j in jsonData:
         addWalker(j, listWalkers)
 
+    #for each walker, plot the graphs and the info
     for w in listWalkers:
         cMRU += 1 if w.movType == "MRU" else 0
         cMRUV += 1 if w.movType == "MRUV" else 0
@@ -27,6 +28,7 @@ def main():
         w.plotGraph()
         plotInfo(w = w)
 
+    #Plot the mean acceleration and the mean velocity of everyone
     mVel /= cMRU
     mAcc /= cMRUV
     plotInfo(cTitle = "Resumo MRU", cMsg = f"Velocidade Média: {round(mVel, 3)} m/s")
@@ -51,7 +53,7 @@ class Walker:
 
     def __estimateIniVel(self, run):
         '''
-        Solves the sistem of equations
+        Solves the system of equations
         ┌ a*t1^2 + b*t1 = s1
         ┤
         └ a*t2^2 + b*t2 = s2
@@ -85,6 +87,12 @@ class Walker:
         return f
 
     def __velocityF(self, run):
+        '''
+        Calculates the velocity function v(t), that for a given t
+        returns the velocity of that time.
+
+        Note that this returns a closure.
+        '''
         tf = self.__finalTime(run)
         v0 = self.__estimateIniVel(run)
         simAccel = 2*(Walker.SPACE-v0*tf)/tf**2
@@ -96,6 +104,7 @@ class Walker:
         if it's an Alternate or Normal read.
         '''
         msr = [float(m) for m in run['measures'].split("|") if m != '']
+        #Takes the average of opposing time measurements
         if(run['mType'] == 'N'):
             msr = [(i+j)/2 for i, j in zip(msr[:len(msr)//2],msr[len(msr)//2:])]
         return msr
@@ -115,14 +124,18 @@ class Walker:
         return [spcList, xticks]
 
     def __finalTime(self, run):
+        '''
+        Returns the final time of a run, given by the csv data.
+        '''
         fTime = run["tcsv"][1] - run["tcsv"][0]
-        # Semantic way of getting the final time...
         return fTime
 
     def __plotCsv(self, run, axis):
         '''
-        Receives a run, and plot the CSV in a given axis!
-        It removes labels, set a xlim, and add legend.
+        Receives a run, and plot the CSV in a given axis, using
+        the pandas library.
+
+        It removes labels, set a xlim, and add the legend.
         '''
         file = "./CSV/" + run['csv'] + ".csv"
         labels = ['Tempo', 'Fx', 'Fy', 'Fz', 'Fr']
@@ -140,8 +153,8 @@ class Walker:
 
     def __setAnnotations(self, **kwargs):
         '''
-        Plot the measured time, the calculated error, and the
-        mean velocity, if it's MRU, or the mean acceleration, if it's MRUV.
+        Plot the measured times in the graph, with an arrow
+        indicating each of them
         '''
         # Get arguments
         xObs = kwargs['xObs']
@@ -160,22 +173,27 @@ class Walker:
 
     def __calculateError(self, xObs, yObs, xt):
         '''
-        Calculates the mean error of a list of points in the form (xObs, yObs) and
-        the points (xObs, xt(xObs)), where xt is a function provided by the
-        caller of the method.
+        Calculates the mean error of a list of points in the
+        form (xObs, yObs) and the points (xObs, xt(xObs)), where xt
+        is a function.
         '''
         # This is an abstract way of calculating the errors,
-        # it can be used either with MRU or MRUV
+        # it can be used either with MRU or MRUV!
         error = [abs(y - xt(x)) for x, y in  zip(xObs, yObs)]
         return sum(error)/len(error)
 
     def getAcceleration(self):
+        '''
+        Returns the mean acceleartion of a Walker in all
+        of the runs.
+        Should only be used with MRUV movements!
+        '''
         mAcc = [self.__velocityF(run)(1) for run in self.times]
         return sum(mAcc)/len(mAcc)
 
     def getVelocity(self):
         '''
-        Returns the mean velocity of a Walker in all of his runs!
+        Returns the mean velocity of a Walker in all of the runs.
         Should only be used with MRU movements!
         '''
         mVel = [self.__spaceF(run)(1) for run in self.times]
@@ -185,53 +203,53 @@ class Walker:
         '''
         Plots the full simulation and data of an MRU movement in a matplotlib plot!
         '''
-        if self.movType == "MRU":
-            for run in self.times:
-                # Simulated: xt - space Function , (x, y) simulated graph
-                xt = self.__spaceF(run)
-                x = np.asarray([0, Walker.XLIM])
-                y = list(map(xt, x))
-                # Observed: (realX, realY) observed points in action!
-                realX = self.__timeList(run)
-                realY = self.__spaceList(run)[0]
-                # Labels and Xticks
-                timeNames = self.__spaceList(run)[1]
-                xticks = [int(i) for i in np.arange(0, self.__finalTime(run), 5)]
-                labels = [str(i) for i in xticks]
-                # Create the plot, and do matplotlib stuff
-                f, axarr = plt.subplots(nrows=2, ncols=1, figsize=(15, 10))
-                ax1 = axarr[0]
-                ax1.plot(x, y, label = "simulado")
-                for i, j in zip(realX, realY):
-                    ax1.plot([i, i], [0, j], 'r--')
-                ax1.scatter(realX, realY,color='red', marker="+", label = "observado")
-                ax1.set_xlabel('tempo (s)', fontsize=13)
-                ax1.set_ylabel('espaço (m)', fontsize=13)
-                ax1.set_title(self.movType+" - "+run["csv"], fontsize=16, color="#000c3d")
-                ax1.set_xticklabels(labels)
-                ax1.set_xticks(xticks)
-                ax1.spines['right'].set_visible(False)
-                ax1.spines['top'].set_visible(False)
-                ax1.set_xlim(0, self.__finalTime(run) + 1)
-                ax1.set_ylim(0, Walker.SPACE + 1)
-                err = self.__calculateError(realX, realY, xt)
-                ax1.scatter(0, 0,  c = 'w', label=f"Velocidade Média: {round(xt(1), 3)} m/s")
-                ax1.scatter(0, 0,  c = 'w', label=f"Erro: {round(err, 2)}")
-                leg = axarr[0].legend(loc ="upper left")
-                plt.draw()
-                p = leg.get_window_extent()
-                # Annotate our plot
-                self.__setAnnotations( xObs = realX, yObs = realY, labels = timeNames,
-                    values = realX, ax = axarr[0])
-                # Plot a CSV in the second axis
-                self.__plotCsv(run, axarr[1])
-                plt.autoscale(False)
-                plt.tight_layout(pad=4, w_pad=0.5, h_pad=5.0)
-                # Show the final plot! :)
-                plt.show()
-
-        else:
+        if(self.movType == "MRUV"):
             self.__plotGraphMRUV()
+            return
+
+        for run in self.times:
+            # Simulated: xt - space Function , (x, y) simulated graph
+            xt = self.__spaceF(run)
+            x = np.asarray([0, Walker.XLIM])
+            y = list(map(xt, x))
+            # Observed: (realX, realY) observed points in action!
+            realX = self.__timeList(run)
+            realY = self.__spaceList(run)[0]
+            # Labels and Xticks
+            timeNames = self.__spaceList(run)[1]
+            xticks = [int(i) for i in np.arange(0, self.__finalTime(run), 5)]
+            labels = [str(i) for i in xticks]
+            # Create the plot, and do matplotlib stuff
+            f, axarr = plt.subplots(nrows=2, ncols=1, figsize=(15, 10))
+            ax1 = axarr[0]
+            ax1.plot(x, y, label = "simulado")
+            for i, j in zip(realX, realY):
+                ax1.plot([i, i], [0, j], 'r--')
+            ax1.scatter(realX, realY,color='red', marker="+", label = "observado")
+            ax1.set_xlabel('tempo (s)', fontsize=13)
+            ax1.set_ylabel('espaço (m)', fontsize=13)
+            ax1.set_title(self.movType+" - "+run["csv"], fontsize=16, color="#000c3d")
+            ax1.set_xticklabels(labels)
+            ax1.set_xticks(xticks)
+            ax1.spines['right'].set_visible(False)
+            ax1.spines['top'].set_visible(False)
+            ax1.set_xlim(0, self.__finalTime(run) + 1)
+            ax1.set_ylim(0, Walker.SPACE + 1)
+            err = self.__calculateError(realX, realY, xt)
+            ax1.scatter(0, 0,  c = 'w', label=f"Velocidade Média: {round(xt(1), 3)} m/s")
+            ax1.scatter(0, 0,  c = 'w', label=f"Erro: {round(err, 2)}")
+            leg = axarr[0].legend(loc ="upper left")
+            plt.draw()
+            p = leg.get_window_extent()
+            # Annotate our plot
+            self.__setAnnotations( xObs = realX, yObs = realY, labels = timeNames,
+                values = realX, ax = axarr[0])
+            # Plot a CSV in the second axis
+            self.__plotCsv(run, axarr[1])
+            plt.autoscale(False)
+            plt.tight_layout(pad=4, w_pad=0.5, h_pad=5.0)
+            # Show the final plot! :)
+            plt.show()
 
     def __plotGraphMRUV(self):
         '''
@@ -239,7 +257,7 @@ class Walker:
         '''
         for run in self.times:
             # Simulated: st - space Function , vt - velocity function
-            #            (xspa, y1) - simulated space graph, (xvel, y2) - simulated velocity graph
+            # (xspa, y1) - simulated space graph, (xvel, y2) - simulated velocity graph
             st = self.__spaceF(run)
             vt = self.__velocityF(run)
             xspa = np.arange(0, Walker.XLIM, 0.01)
@@ -256,7 +274,7 @@ class Walker:
             # Create the plot, and do matplotlib stuff
             f, axarr = plt.subplots(nrows=2, ncols=1, figsize=(15, 10))
             ax1 = axarr[0]
-            ax2 = ax1.twinx()
+            ax2 = ax1.twinx() # secondary shared axis
             ax3 = axarr[1]
             ax1.plot(xspa, y1, label="espaço simulado")
             for i, j in zip(realX, realY):
